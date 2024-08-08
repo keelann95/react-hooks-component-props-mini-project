28002703d54be7b60e1c17eba36ca61d0de1ac44
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-08-08 15:01:33 +0300

    creates new files and updates them

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React from "react";
-import blogData from "../data/blog";
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
 
-console.log(blogData);
+const App = () => {
+  const blogName = 'Underreacted';
+  const blogLogo = 'https://example.com/logo.png';
+  const blogAbout = 'Welcome to our blog about React!';
 
-function App() {
+  const posts = [
+    {
+      id: 1,
+      title: 'Components 101',
+      date: 'December 15, 2020',
+      preview: 'Setting up the building blocks of your site',
+    },
+    {
+      id: 2,
+      title: 'React Data Flow',
+      date: 'December 11, 2020',
+      preview: 'Passing props is never passé',
+    },
+    {
+      id: 3,
+      title: 'Function Components vs Class Components',
+      date: 'December 10, 2020',
+      preview: 'React, meet OOJS.',
+    },
+  ];
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogName} />
+      <About image={blogLogo} about={blogAbout} />
+      <ArticleList posts={posts} />
     </div>
   );
-}
+};
 
 export default App;
